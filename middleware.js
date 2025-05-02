e068f21bb3318c90090deaed099c52f13796fec9
BYIRINGIRO55 <byiringirosamuel533@gmail.com>
2025-05-02 12:53:41 +0100

    midleware update

--- middleware.js
+++ middleware.js
@@ -40,7 +40,7 @@ export const config = {
 } */
 
 
-  import { authMiddleware } from "@clerk/nextjs";
+ /*  import { authMiddleware } from "@clerk/nextjs/server";
  
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -49,6 +49,17 @@ export default authMiddleware({
   publicRoutes: ["/api/webhook"]
 });
  
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+}; */
+
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+// Protects all routes, except public ones like /api/webhook
+export default clerkMiddleware({
+  publicRoutes: ["/api/webhook"],
+});
+
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
